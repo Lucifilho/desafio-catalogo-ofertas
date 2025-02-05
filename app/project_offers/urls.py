10b0845b5a370aff16766701be89ac33f20aabdb
Luciel Filho <lucielfilho@hotmail.com>
2025-02-04 23:03:26 -0300

    Piloto
    Adiciona piloto do projeto

---
+++ app/project_offers/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from app_offers import views
+
+urlpatterns = [
+
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+
+]
