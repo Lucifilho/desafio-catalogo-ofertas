6b81956c8604fe75b96ce120b794b46f3b50fdf4
Luciel Filho <lucielfilho@hotmail.com>
2025-02-10 18:20:42 -0300

    Finaliza o Projeto

---
+++ app/app_offers/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Nome do Produto")
+    link = models.URLField(null=True, blank=True, verbose_name="Link")
+    pricePrevious = models.CharField(max_length=255, null=True, blank=True, verbose_name="Preço Anterior")
+    installment = models.CharField(max_length=255, null=True, blank=True, verbose_name="Parcelamento")
+    price = models.FloatField(null=True, blank=True, verbose_name="Preço")
+    shipping_type = models.CharField(max_length=255, null=True, blank=True, verbose_name="Tipo de Entrega")
+    free_shipping = models.CharField(max_length=255, null=True, blank=True, verbose_name="Frete Grátis")
+    percentual_discount = models.IntegerField(null=True, blank=True, verbose_name="Percentual de Desconto")
+    image = models.URLField(null=True, blank=True, verbose_name="Imagem")
+
+    class Meta:
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
+
+    def __str__(self):
+        return self.name
---
+++ app/app_offers/views.py
@@ -0,0 +1,121 @@
+import re
+from django.shortcuts import render, redirect
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+from .models import Product
+
+def get_products(request):
+    driver = webdriver.Chrome()
+
+    driver.get("https://www.mercadolivre.com.br/")
+
+    pageTitle = driver.title
+
+    elem = driver.find_element(By.NAME, "as_word")
+    elem.clear()
+    elem.send_keys("Computador Gamer i7 16gb ssd 1tb")
+
+    search = driver.find_element(By.XPATH, "/html/body/header/div/div[2]/form/button")
+    search.click()
+
+    # Rolar a página para garantir que todos os elementos sejam carregados
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)  # Aguarde o carregamento da página
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    # Espera explícita para garantir que as imagens estão visíveis
+    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "poly-component__picture")))
+
+    elementsImage = driver.find_elements(By.CLASS_NAME, "poly-component__picture")
+    elementsName = driver.find_elements(By.CLASS_NAME, "poly-component__title")
+    elementsInstallment = driver.find_elements(By.CLASS_NAME, "poly-price__installments")
+    elementsLink = driver.find_elements(By.CLASS_NAME, "poly-component__title")
+    elementsPricePrevious = driver.find_elements(By.CSS_SELECTOR, ".andes-money-amount.andes-money-amount--previous.andes-money-amount--cents-comma")
+    elementsPrice = driver.find_elements(By.CSS_SELECTOR, ".poly-price__current .andes-money-amount.andes-money-amount--cents-superscript .andes-money-amount__fraction")
+    elementsTypeShipping = driver.find_elements(By.CLASS_NAME, "poly-component__shipped-from")
+    elementsFreeShipping = driver.find_elements(By.CLASS_NAME, "poly-component__shipping")
+    elementsPercentualDiscount = driver.find_elements(By.CLASS_NAME, "andes-money-amount__discount")
+
+    products = []
+    for i in range(len(elementsName)):
+        name = elementsName[i].text
+
+        # Tentando pegar a imagem do atributo 'data-src' ou 'src'
+        image = elementsImage[i].get_attribute('data-src') or elementsImage[i].get_attribute('src')
+
+        # Verifica se a imagem foi carregada corretamente ou se é um placeholder
+        if not image or image.startswith("data:image"):
+            image = "https://via.placeholder.com/150"  # URL de placeholder
+
+        url = elementsLink[i].get_attribute('href')
+        pricePrevious = elementsPricePrevious[i].get_attribute("aria-label") if i < len(elementsPricePrevious) and \
+                                                                                elementsPricePrevious[i].get_attribute(
+                                                                                    "aria-label") else None
+        price_text = elementsPrice[i].text if i < len(elementsPrice) else None
+        if price_text:
+            price_text = price_text.replace('.', '').replace(',', '.')
+            price = float(price_text)
+        else:
+            price = None
+
+        installment_quantity = elementsInstallment[i].text if i < len(elementsInstallment) else None
+        shipping_type = "Entrega Full" if i < len(elementsTypeShipping) and elementsTypeShipping[i].text else "Entrega Normal"
+        free_shipping = "Frete Grátis" if i < len(elementsFreeShipping) else "Consulte o valor do frete"
+        percentual_discount_text = elementsPercentualDiscount[i].text if i < len(elementsPercentualDiscount) else None
+        percentual_discount = int(re.search(r'\d+', percentual_discount_text).group()) if percentual_discount_text else None
+
+        # Criar o objeto Product com as informações obtidas
+
+
+        product = Product(
+            name=name,
+            link=url,
+            pricePrevious=pricePrevious,
+            image=image,
+            installment=installment_quantity,
+            price=price,
+            shipping_type=shipping_type,
+            free_shipping=free_shipping,
+            percentual_discount=percentual_discount,
+        )
+        product.save()
+        products.append(product)
+
+    #driver.quit()
+
+    return redirect('home')
+
+def home(request):
+    free_shipping_filter = request.GET.get('free_shipping', 'all')
+    sort_option = request.GET.get('sort', 'default')
+    shipping_type_filter = request.GET.get('shipping_type', 'all')
+
+    products = Product.objects.all()
+
+    if free_shipping_filter == 'Frete Grátis':
+        products = products.filter(free_shipping="Frete Grátis")
+
+    if shipping_type_filter != 'all':
+        products = products.filter(shipping_type=shipping_type_filter)
+
+    if sort_option == 'highest_price':
+        products = products.order_by('-price')
+    elif sort_option == 'lowest_price':
+        products = products.order_by('price')
+    elif sort_option == 'highest_discount':
+        products = products.order_by('-percentual_discount')
+
+    return render(request, 'catalog/catalog_offerings.html', {
+        'pageTitle': 'Product Catalog',
+        'products': products,
+        'free_shipping_filter': free_shipping_filter,
+        'sort_option': sort_option
+    })
